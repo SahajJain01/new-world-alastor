bcf12d8b8443d9e9c6e0ff86c29af38a19d56722
Sahaj Jain <jainsahaj@gmail.com>
2021-10-06 19:59:43 +0530

    fixes

--- index.js
+++ index.js
@@ -1,55 +1,20 @@
 const Discord = require("discord.js");
 const https = require('https');
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-var url = 'https://nwdb.info/server-status/data.json';
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+var url = 'https://nwdb.info/server-status/data.json';
+var worldName = 'Alastor';
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
-// client.on('interactionCreate', async interaction => {
-//   if (!interaction.isCommand()) return;
-
-//   if (interaction.commandName === 'ping') {
-//     await interaction.reply('Pong!');
-//   }
-// });
-
 client.login('ODEyMjI0MjU0OTIxNDA4NTI1.YC9oyA.o5TMzTeEoNTMDt7oLn5LlRR3gFI');
 
-// app.get('/', function (req, r) {
-//     https.get(url, function (res) {
-//         var body = '';
-//         res.on('data', function (chunk) {
-//             body += chunk;
-//         });
-//         res.on('end', function () {
-//             var data = JSON.parse(body);
-//             data.data.servers.forEach(e => {
-//                 if (e.worldName == req.query.worldName) {
-//                     r.send(e);
-//                     client.channels.cache.get('864708503058251796').setTopic('Testing')
-//                         .then(newChannel => console.log(`Channel's new topic is ${newChannel.topic}`))
-//                         .catch(console.error);
-//                 }
-//             });
-//         });
-//     }).on('error', function (e) {
-//         console.log("Error fetching json: ", e);
-//     });
+setInterval(fetchDataAndUpdateTopic, 600000);
 
-// });
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`)
-// })
-
-setInterval(function(){ 
+function fetchDataAndUpdateTopic() {
     https.get(url, function (res) {
         var body = '';
         res.on('data', function (chunk) {
@@ -58,21 +23,35 @@ setInterval(function(){
         res.on('end', function () {
             var data = JSON.parse(body);
             data.data.servers.forEach(e => {
-                if (e.worldName == 'Alastor') {
+                if (e.worldName == worldName) {
                     var st;
-                    if(e.status == 8) {
-                        st = 'Online';
-                    } else {
-                        st = 'Offline';
+                    switch (e.status) {
+                        case 12:
+                            st = 'Maintenance'
+                            break;
+                        case 8:
+                            st = 'Online/Full'
+                            break;
+                        case 4:
+                            st = 'Online'
+                            break;
+                        case 0:
+                            st = 'Offline'
+                            break;
+                        default:
+                            st = 'Unknown'
+                            break;
                     }
-                    client.channels.cache.get('864708503058251796').setTopic
-                        (e.worldName + ' - ' + e.connectionCount + '/' + e.connectionCountMax + ', Queue: ' + e.queueCount + ', Queue Time: ' + e.queueTime + 's' + ', Status: ' + st + ', StatusCode: ' + e.status)
-                            .then(newChannel => console.log(`Channel's new topic is ${newChannel.topic}`))
-                            .catch(console.error);
+                    var t = new Date(e.queueTime * 1000).toISOString().substr(11, 8);
+                    var str = e.worldName + ' - ' + (e.connectionCount + e.queueCount) + '/' + e.connectionCountMax + ' | Wait: ' + t + ' | Status: ' + st + ' | StatusCode: ' + e.status + ' | Last update: ' + new Date(Date.now()).toLocaleString("en-IN", { timeZone: 'Asia/Kolkata' });
+                    console.log('Sending update to discord: ' + str);
+                    client.channels.cache.get('864708503058251796').setTopic(str)
+                        .then(newChannel => console.log(`Channel's new topic is ${newChannel.topic}`))
+                        .catch(console.error);
                 }
             });
         });
     }).on('error', function (e) {
         console.log("Error fetching json: ", e);
     });
- }, 60000);
+}
